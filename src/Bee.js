4028716cb605a0426fd10775f09854078936dae7
Joy <parkerjn90@gmail.com>
2022-04-18 11:42:00 -0400

    create pseudoclass Bee by updating age and color, adding job, maintaining eat method and food

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,15 @@
 var Bee = function() {
+
+
+  this.food = growUp.food;
+  // this.eat = growUp.eat;
+
 };
 
+var growUp = new Grub();
+
+Bee.prototype.age = 5;
+Bee.prototype.color = 'yellow';
+Bee.prototype.job = 'keep on growing';
+Bee.prototype.eat = growUp.eat;
+Bee.prototype.food = growUp.food;
