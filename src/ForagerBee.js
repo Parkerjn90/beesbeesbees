ebe62754bb79c059f2426ba86dbad28876fb8e8d
Joy <parkerjn90@gmail.com>
2022-04-18 11:58:46 -0400

    create ForagerBee subclass that updated age and job property, maintained color and food properties and eat method, and added canFly and treasureChest properties and forage methods

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,16 @@
 var ForagerBee = function() {
+  this.age = 10;
+  this.color = newJob.color;
+  this.job = 'find pollen';
+  this.food = newJob.food;
+  this.canFly = true;
+  this.treasureChest = [];
 };
 
+var newJob = new Bee();
+
+ForagerBee.prototype.eat = newJob.eat;
+
+ForagerBee.prototype.forage = function(treasure) {
+  this.treasureChest.push(treasure);
+};
