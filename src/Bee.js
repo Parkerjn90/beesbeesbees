5481cd9b844eec7a3fb29bcd551380dc0856a4bf
Joy <parkerjn90@gmail.com>
2022-04-18 11:49:04 -0400

    ensure instantiation of Bee includes the 'this' keyword

--- src/Bee.js
+++ src/Bee.js
@@ -1,11 +1,11 @@
 var Bee = function() {
-
+  this.food = growUp.food;
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
 };
 
 var growUp = new Grub();
 
-Bee.prototype.age = 5;
-Bee.prototype.color = 'yellow';
 Bee.prototype.job = 'keep on growing';
 Bee.prototype.eat = growUp.eat;
-Bee.prototype.food = growUp.food;
