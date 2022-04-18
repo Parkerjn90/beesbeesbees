37cb31ea3df9021fd2cfaf7455a961afcd37e006
Joy <parkerjn90@gmail.com>
2022-04-18 11:53:13 -0400

    creat HoneyMakerBee subclass that updates age and job properties, maintains color, food properties and eat method, and adds honeyPot property and makeHoney and giveHoney methods

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,21 @@
 var HoneyMakerBee = function() {
+  this.age = 10;
+  this.job = 'make honey';
+  this.color = newJob.color;
+  this.food = newJob.food;
+  this.honeyPot = 0;
 };
 
+var newJob = new Bee();
+
+HoneyMakerBee.prototype.eat = newJob.eat;
+
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot++;
+};
+
+HoneyMakerBee.prototype.giveHoney = function() {
+  if (this.honeyPot > 0) {
+    this.honeyPot--;
+  }
+};
